62f38047fc515915901e682b0528daaf6d863497
Akshay <akshay.gautam@in.g4s.com>
2018-06-10 10:49:05 +0530

    crosscall

--- src/main/java/com/akshay/spring/controllers/StudentController.java
+++ src/main/java/com/akshay/spring/controllers/StudentController.java
@@ -1,9 +1,9 @@
 package com.akshay.spring.controllers;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +16,7 @@ import com.akshay.spring.services.StudentService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 	
 	@Autowired

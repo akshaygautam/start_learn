9a7b2ca07fad9f4a64cef6c31f4804142370099d
akshaygautam <akshay.gautam2209@gmail.com>
2018-07-04 23:47:20 -0700

    Revert "Added Spring security"
    This reverts commit b6fced750b3a309ed2e6e80f995bf5b0023b06eb.

--- src/main/java/com/akshay/spring/repositories/StudentRepository.java
+++ src/main/java/com/akshay/spring/repositories/StudentRepository.java
@@ -1,10 +1,8 @@
 package com.akshay.spring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.akshay.spring.models.StudentModel;
-@Repository
+
 public interface StudentRepository extends JpaRepository<StudentModel, Long> {
 
 	StudentModel findByRollNumber(String rollNumber);
--- src/main/java/com/akshay/spring/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.akshay.spring.services;
-
-import com.akshay.spring.models.User;
-
-public interface UserService {
-	public User findUserByEmail(String email);
-	public void saveUser(User user);
-}
